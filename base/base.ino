1148366c57e6dded118124ab76564586726d4c2d
Mateusz Kopaszewski <91031647+mateuszk0pa@users.noreply.github.com>
2022-04-07 18:51:52 +0200

    Create base.ino

---
+++ base/base.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);
+  delay(8000);
+  digitalWrite(13, LOW);
+}
